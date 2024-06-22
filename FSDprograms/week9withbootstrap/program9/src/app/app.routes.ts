b70964bc3c63d6bdfbf2d77c97dc091b4bcf0786
devisar <anupenugonda1998@cmritonline.ac.in>
2024-06-22 23:46:26 +0530

    commited

---
+++ FSDprograms/week9withbootstrap/program9/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        title:"Home"
+    },
+    {
+        path:'profile',
+        component:ProfileComponent,
+        title:"Profile"
+    }
+];
